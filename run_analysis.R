6506fb733733fc8ce2a6349c895832c79c6882e0
Brian Gulbis <brian.gulbis@gmail.com>
2015-04-13 14:39:14 -0500

    steps 3-5 and output

--- run_analysis.R
+++ run_analysis.R
@@ -33,14 +33,46 @@ dataSbj <- bind_rows(testSbj, trainSbj) %>% rename(person = V1)
 ## get the column names for the X data, contained in features.txt
 ftr <- read.table("UCI HAR Dataset/features.txt")
 
+## get the labels for each activity
+act <- read.table("UCI HAR Dataset/activity_labels.txt")
+act[[2]] <- tolower(act[[2]])
+
 ## find the columns which are either a mean or standard deviation
-keepX <- grep("mean|std", ftr[[2]])
+## prefix 't' to denote time
+## Note the 'f' to indicate frequency domain signals
+## Acc = accelerometer; Gyro = gyroscope
+## body and gravity acceleration signals
+## eliminate meanFreq
+## "mean time body accelerometer xyz-axis"
+## "stdev time body accelerometer xyz-axis"
+## mag = magnitude
+## strsplit
+
+keepX <- grep("mean\\(|std\\(", ftr[[2]])
+Xnames <- tolower(as.character(ftr[keepX,2]))
+Xnames <- gsub("\\(\\)", "", Xnames)
+Xnames <- gsub("^t", "time-", Xnames)
+Xnames <- gsub("^f", "frequency-", Xnames)
+Xnames <- gsub("acc", "-accelerometer", Xnames)
+Xnames <- gsub("gyro", "-gyroscope", Xnames)
+Xnames <- gsub("std", "stdev-", Xnames)
+Xnames <- gsub("jerk", "-jerk", Xnames)
+Xnames <- gsub("mag", "-magnitude", Xnames)
+Xnames <- gsub("x$", "x-axis", Xnames)
+Xnames <- gsub("y$", "y-axis", Xnames)
+Xnames <- gsub("z$", "z-axis", Xnames)
 
 ## select only those columns that have mean or standard deviation
-data <- select(dataX, keepX)
-colnames(data) <- as.character(ftr[keepX,2])
+data <- select(dataX, keepX) ## %>% rename_(.dots=setNames(nm=Xnames))
+colnames(data) <- Xnames
 data <- bind_cols(dataSbj, dataY, data) %>%
-        mutate(person = as.factor(person), activity = as.factor(activity))
+        mutate(person = factor(person), 
+               activity = factor(activity, levels=act[[1]], labels=act[[2]]))
 
 ## cleanup data frames which are no longer needed
 rm(testX, testY, testSbj, trainX, trainY, trainSbj, dataX, dataY, dataSbj)
+
+## step 5
+results <- group_by(data, person, activity) %>%
+            summarise_each(funs(mean))
+write.table(results, "tidydata.txt", row.names=FALSE)
